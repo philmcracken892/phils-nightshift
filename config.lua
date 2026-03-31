9cd04ba7c34354b4a1822fa092ae1f096d12c506
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2026-03-31 10:26:46 +0100

    Add NPC models and new gun store configuration

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config.Locations = {
         name = "Valentine Saloon",
         coords = vector3(-300.54, 777.18, 118.78),
         npcPositions = {
-            { x = -313.37, y = 806.02, z = 118.98, heading = 300.69 },
+            { x = -313.37, y = 806.02, z = 118.98, heading = 300.69, model = `mes_marston1_males_01` },
            
         }
     },
@@ -13,7 +13,15 @@ Config.Locations = {
         name = "Valentine Store",
         coords = vector3(-328.64, 814.97, 117.4),
         npcPositions = {
-            { x = -324.14, y = 803.61, z = 117.88, heading = 300.69 },
+            { x = -324.14, y = 803.61, z = 117.88, heading = 300.69, model = `cs_fishcollector` },
+           
+        }
+    },
+	{
+        name = "Valentine gun Store",
+        coords = vector3(-251.97, 803.56, 120.58),
+        npcPositions = {
+            { x = -281.58, y = 778.77, z = 119.5, heading = 300.69, model = `A_M_M_RANCHER_01` },
            
         }
     },
